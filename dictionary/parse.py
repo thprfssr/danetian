0003184795d904415ff55fcf8d8dcd28d4b8fba6
Javier Castro <jc@javiercastro.xyz>
2021-02-04 16:47:46 -0500

    modified:   dictionary.latex.liquid modified:   dictionary.yaml new file:   germanic.yaml modified:   parse.py

--- dictionary/parse.py
+++ dictionary/parse.py
@@ -7,16 +7,21 @@ template = 'dictionary.latex.liquid'
 
 with open(dictionary) as f:
     df = yaml.full_load(f)
-keys = sorted(df, key = lambda s: unidecode(s.casefold()))
+df = sorted(df, key = lambda s: unidecode(s['key'].casefold()))
 
 for e in df:
-    if not 'ety' in df[e].keys():
-        df[e]['ety'] = ''
-    if not 'parts' in df[e].keys():
-        df[e]['parts'] = ''
+    if not 'ety' in e.keys():
+        e['ety'] = ''
+    if not 'parts' in e.keys():
+        e['parts'] = ''
+    for item in e['definitions']:
+        if 'tag' not in item.keys():
+            item['tag'] = ''
+        if 'ex' not in item.keys():
+            item['ex'] = ''
 
 with open(template) as f:
     liq = Liquid(f)
 
-ret = liq.render(dictionary = df, keys = keys)
+ret = liq.render(dictionary = df)
 print(ret)

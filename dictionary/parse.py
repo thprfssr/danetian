38cb82f7c91c8634193edef1838a7ce0511326c0
Javier Castro <jc@javiercastro.xyz>
2021-06-05 11:28:15 -0400

    modified:   dictionary.latex modified:   dictionary.latex.liquid modified:   dictionary.pdf modified:   dictionary.yaml modified:   parse.py

--- dictionary/parse.py
+++ dictionary/parse.py
@@ -7,14 +7,14 @@ template = 'dictionary.latex.liquid'
 
 with open(dictionary) as f:
     df = yaml.full_load(f)
-df = sorted(df, key = lambda s: unidecode(s['key'].casefold()))
+df = sorted(df, key = lambda s: unidecode(s['key'].casefold().replace('-', '')))
 
 for e in df:
     if not 'ety' in e.keys():
         e['ety'] = ''
     if not 'parts' in e.keys():
         e['parts'] = ''
-    for item in e['definitions']:
+    for item in e['content']:
         if 'tag' not in item.keys():
             item['tag'] = ''
         if 'ex' not in item.keys():
